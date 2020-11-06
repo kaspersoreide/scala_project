4a53b665fe482a3e78d86373daf5c21eee6e164c
le0-0 <leonard.schoyen@gmail.com>
2020-11-06 19:42:33 +0100

    Reformat code.

--- project_tasks/src/main/scala/Account.scala
+++ project_tasks/src/main/scala/Account.scala
@@ -1,41 +1,14 @@
 package scala
 
-import exceptions._
-
 class Account(val bank: Bank, initialBalance: Double) {
-
-  class Balance(private var amount: Double) {
-    /** Returns whether this balance is less than amount.
-     *
-     * Makes no changes to balance.
-     */
-    def <(amount: Double): Boolean = {
-      this.amount < amount
-    }
-
-    /** Returns whether this balance is less than supplied balance.
-     *
-     * Makes no changes to balance.
-     */
-    def <(balance: Balance): Boolean = {
-      amount < balance.amount
-    }
-
-    /** Add supplied amount to balance.
-     *
-     * Amount can be negative. Thread-safe. Cannot cause deadlock.
-     */
-    def changeBalance(amount: Double): Unit = this.synchronized({
-      this.amount = amount
-    })
-  }
 
   val balance: Balance = new Balance(initialBalance)
 
   /** Remove supplied amount from this account.
    *
    * Thread-safe. Cannot cause deadlock.
-   * @return Either a left with unit if successful, or a right with a error message otherwise.
+   *
+   * @return Either a left with the unit if successful, or a right with a error message otherwise.
    */
   def withdraw(amount: Double): Either[Unit, String] = {
 
@@ -48,15 +21,16 @@ class Account(val bank: Bank, initialBalance: Double) {
     }
 
     Left[Unit, String](balance.changeBalance(-amount))
-    
+
   }
 
   /** Add supplied amount to this account.
    *
    * Thread-safe. Cannot cause deadlock.
-   * @return Either a left with unit if successful, or a right with a error message otherwise.
+   *
+   * @return Either a left with the unit if successful, or a right with a error message otherwise.
    */
-  def deposit (amount: Double): Either[Unit, String] = {
+  def deposit(amount: Double): Either[Unit, String] = {
 
     if (amount < 0) {
       return Right[Unit, String]("Amount to be deposited cannot be negative.")
@@ -66,11 +40,51 @@ class Account(val bank: Bank, initialBalance: Double) {
 
   }
 
-  def getBalanceAmount: Double = ???
-
-  def transferTo(account: Account, amount: Double): Unit = {
-    bank addTransactionToQueue (this, account, amount)
+  /** Returns amount in the balance of this account.
+   *
+   * Makes no changes to this account.
+   */
+  def getBalanceAmount: Double = {
+    balance.amount
   }
 
+  def transferTo(account: Account, amount: Double): Unit = {
+    bank.addTransactionToQueue(this, account, amount)
+  }
+
+  class Balance(private var _amount: Double) {
+
+    /** Returns whether this balance is less than amount.
+     *
+     * Makes no changes to this balance.
+     */
+    def <(amount: Double): Boolean = {
+      this._amount < amount
+    }
+
+    /** Returns whether this balance is less than supplied balance.
+     *
+     * Makes no changes to this balance.
+     */
+    def <(balance: Balance): Boolean = {
+      _amount < balance._amount
+    }
+
+    /** Add supplied amount to this balance.
+     *
+     * Amount can be negative. Thread-safe. Cannot cause deadlock.
+     */
+    def changeBalance(amount: Double): Unit = this.synchronized({
+      this._amount += amount
+    })
+
+    /** Returns amount in this balance.
+     *
+     * Makes no changes to this balance.
+     */
+    def amount: Double = {
+      _amount
+    }
+  }
 
 }
