8d7e88f5108c23f45b8b542e8b3ffccf92f8a4a3
le0-0 <leonard.schoyen@gmail.com>
2020-11-06 19:07:54 +0100

    Changed indenting to 2 space tabs. Implemented drafts for withdraw and deposit in Account.

--- project_tasks/src/main/scala/Account.scala
+++ project_tasks/src/main/scala/Account.scala
@@ -4,20 +4,73 @@ import exceptions._
 
 class Account(val bank: Bank, initialBalance: Double) {
 
-    class Balance(var amount: Double) {}
-
-    val balance = new Balance(initialBalance)
-
-    // TODO
-    // for project task 1.2: implement functions
-    // for project task 1.3: change return type and update function bodies
-    def withdraw(amount: Double): Unit = ???
-    def deposit (amount: Double): Unit = ???
-    def getBalanceAmount: Double       = ???
-
-    def transferTo(account: Account, amount: Double) = {
-        bank addTransactionToQueue (this, account, amount)
+  class Balance(private var amount: Double) {
+    /** Returns whether this balance is less than amount.
+     *
+     * Makes no changes to balance.
+     */
+    def <(amount: Double): Boolean = {
+      this.amount < amount
     }
+
+    /** Returns whether this balance is less than supplied balance.
+     *
+     * Makes no changes to balance.
+     */
+    def <(balance: Balance): Boolean = {
+      amount < balance.amount
+    }
+
+    /** Add supplied amount to balance.
+     *
+     * Amount can be negative. Thread-safe. Cannot cause deadlock.
+     */
+    def changeBalance(amount: Double): Unit = this.synchronized({
+      this.amount = amount
+    })
+  }
+
+  val balance: Balance = new Balance(initialBalance)
+
+  /** Remove supplied amount from this account.
+   *
+   * Thread-safe. Cannot cause deadlock.
+   * @return Either a left with unit if successful, or a right with a error message otherwise.
+   */
+  def withdraw(amount: Double): Either[Unit, String] = {
+
+    if (amount < 0) {
+      return Right[Unit, String]("Amount to be withdrawn cannot be negative.")
+    }
+
+    if (balance < amount) {
+      return Right[Unit, String]("Amount to be withdrawn cannot be higher than account balance.")
+    }
+
+    Left[Unit, String](balance.changeBalance(-amount))
+    
+  }
+
+  /** Add supplied amount to this account.
+   *
+   * Thread-safe. Cannot cause deadlock.
+   * @return Either a left with unit if successful, or a right with a error message otherwise.
+   */
+  def deposit (amount: Double): Either[Unit, String] = {
+
+    if (amount < 0) {
+      return Right[Unit, String]("Amount to be deposited cannot be negative.")
+    }
+
+    Left[Unit, String](balance.changeBalance(amount))
+
+  }
+
+  def getBalanceAmount: Double = ???
+
+  def transferTo(account: Account, amount: Double): Unit = {
+    bank addTransactionToQueue (this, account, amount)
+  }
 
 
 }
