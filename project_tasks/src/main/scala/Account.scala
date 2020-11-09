01be2474c868790f4d37162a310a6a87b509fa28
le0-0 <leonard.schoyen@gmail.com>
2020-11-09 17:21:11 +0100

    Removed unnecessary block bodies in Account methods. Added the Unit to constructors of Left in Account methods.

--- project_tasks/src/main/scala/Account.scala
+++ project_tasks/src/main/scala/Account.scala
@@ -9,30 +9,27 @@ class Account(val bank: Bank, initialBalance: Double) {
     // TODO
     // for project task 1.2: implement functions
     // for project task 1.3: change return type and update function bodies
-    def withdraw(amount: Double): Either[Unit, String] = {
-        this.synchronized {
-            if (amount > this.balance.amount || amount < 0) {
-                return Right("Error: Invalid withdrawal (results in negative amount)!")
-            }
-            this.balance.amount -= amount
-            Left()
+    def withdraw(amount: Double): Either[Unit, String] = this.synchronized {
+        if (amount > this.balance.amount || amount < 0) {
+            return Right("Error: Invalid withdrawal (results in negative amount)!")
         }
+        this.balance.amount -= amount
+        Left(Unit)
     }
 
-    def deposit (amount: Double): Either[Unit, String] = {
-        this.synchronized {
-            if (amount <= 0) {
-                return Right("Error: amount is 0 or negative")
-            }
-            this.balance.amount += amount   
-            Left() 
-        }   
-    }
+    def deposit (amount: Double): Either[Unit, String] = this.synchronized({
+        if (amount <= 0) {
+            return Right("Error: amount is 0 or negative")
+        }
+        this.balance.amount += amount
+        Left(Unit)
+    })
+
     def getBalanceAmount: Double = {
         this.balance.amount
     }
 
-    def transferTo(account: Account, amount: Double) = {
+    def transferTo(account: Account, amount: Double): Unit = {
         bank.addTransactionToQueue (this, account, amount)
     }
 }
